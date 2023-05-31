311751371176f3a94c42bd50325a5fc617419fe7
Fabian Perez <fperezv@payqa.dev>
2023-05-30 20:21:01 -0500

    Corrección de conexión a la base de datos

--- ProyectoGrupal/Utils/DatabaseUtils.cs
+++ ProyectoGrupal/Utils/DatabaseUtils.cs
@@ -14,12 +14,12 @@ public class DatabaseUtils
 
     public SqlConnection getConnection(DatabaseConnection databaseConnection)
     {
-        var urlConnection = databaseConnection switch
+        var connectionName = databaseConnection switch
         {
             DatabaseConnection.ULE_PUENTE_PIEDRA1 => "Server=(local);DataBase=Ule_Puente_Piedra1;TrustServerCertificate=true;uid=sa;pwd=MSSQL.01062017",
             DatabaseConnection.PROYECTO_MUNICIPALIDAD => "Server=(local);DataBase=Proyecto_Municipalidad;TrustServerCertificate=true;uid=sa;pwd=MSSQL.01062017",
             _ => throw new NotSupportedException("Connection Not Supported")
         };
-        return new SqlConnection(urlConnection);
+        return new SqlConnection(_configuration["ConnectionStrings:" + connectionName]);
     }
 }
